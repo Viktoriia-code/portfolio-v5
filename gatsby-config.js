04d87fd1baef07e3899adb348adb252f6f1f2c63
Viktoriia <beloborodova.viktoriia@gmail.com>
2025-01-03 13:46:49 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `portfolio-v5`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-sitemap"]
+};
